af458011db6d87903dd5b2da82600bd95c3fdfc2
許嘉玲 <stephanie@xujialingdeMacBook-Pro.local>
2026-02-17 16:43:40 +0800

    導入 SSR 與 ISR 資料抓取策

--- lib/tmdb.ts
+++ lib/tmdb.ts
@@ -3,7 +3,7 @@ const BASE_URL = "https://api.themoviedb.org/3"
 export async function getPopularMovies() {
   const today = new Date().toISOString().split('T')[0];
   const res = await fetch(
-    `${BASE_URL}/movie/popular?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&primary_release_date.lte=${today}`
+    `${BASE_URL}/movie/popular?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&primary_release_date.lte=${today}`,{ next: { revalidate: 3600 } }
   )
   if (!res.ok) throw new Error("Failed to fetch")
   return res.json()
@@ -12,7 +12,7 @@ export async function getPopularMovies() {
 export async function searchMovies(query: string) {
   const today = new Date().toISOString().split('T')[0];
   const res = await fetch(
-    `${BASE_URL}/search/movie?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&query=${query}&primary_release_date.lte=${today}`
+    `${BASE_URL}/search/movie?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&query=${query}&primary_release_date.lte=${today}`,{ cache: 'no-store' }
   )
   return res.json()
 }
@@ -20,7 +20,7 @@ export async function searchMovies(query: string) {
 export async function getSortedMovies(sort: string) {
   const today = new Date().toISOString().split('T')[0];
   const res = await fetch(
-    `${BASE_URL}/discover/movie?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&sort_by=${sort}&primary_release_date.lte=${today}`
+    `${BASE_URL}/discover/movie?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&sort_by=${sort}&primary_release_date.lte=${today}`,{ cache: 'no-store' }
   )
 
   if (!res.ok) throw new Error("Failed to fetch")
