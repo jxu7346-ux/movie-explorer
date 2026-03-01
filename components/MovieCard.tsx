0493aaab5057fe68e16d3c92efd21d383c83cf5a
許嘉玲 <stephanie@xujialingdeMacBook-Pro.local>
2026-03-01 14:10:37 +0800

    將 <img> 改為 next/image 以優化載入速度

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,13 +1,17 @@
 import Link from "next/link"
 import { Movie } from "@/types/movie"
+import Image from "next/image"
 
 export default function MovieCard({ movie }: { movie: Movie }) {
   return (
     <Link href={`/movie/${movie.id}`}>
       <div style={{backgroundColor:"oklch(98.5% 0.002 247.839)"}} className="rounded-lg overflow-hidden hover:scale-105 transition">
-        <img
+       <Image 
           src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
           alt={movie.title}
+          width={500}
+          height={750}
+          className="object-cover"
         />
         <div className="p-3">
           <h2 className="text-sm font-semibold">{movie.title}</h2>
